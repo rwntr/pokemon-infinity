551c53cb22fbdbbb947bceb76037a495e8090c43
ru <ruairiwinter@gmail.com>
2024-09-11 21:01:48 -0500

    Fixed the first batch of ability & interaction tests for CI & added vengeance.c, all ability tests are passing

--- test/battle/ability/blaze.c
+++ test/battle/ability/blaze.c
@@ -4,11 +4,14 @@
 SINGLE_BATTLE_TEST("Blaze boosts Fire-type moves in a pinch", s16 damage)
 {
     u16 hp;
-    PARAMETRIZE { hp = 99; }
-    PARAMETRIZE { hp = 33; }
+    u16 ability;
+
+    PARAMETRIZE { ability = ABILITY_SHIELD_DUST; hp = 33; }
+    PARAMETRIZE { ability = ABILITY_BLAZE; hp = 33; }
+
     GIVEN {
         ASSUME(gMovesInfo[MOVE_EMBER].type == TYPE_FIRE);
-        PLAYER(SPECIES_CHARMANDER) { Ability(ABILITY_BLAZE); MaxHP(99); HP(hp); }
+        PLAYER(SPECIES_CHARMANDER) { Ability(ability); MaxHP(99); HP(hp); }
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
         TURN { MOVE(player, MOVE_EMBER); }
@@ -21,18 +24,21 @@ SINGLE_BATTLE_TEST("Blaze boosts Fire-type moves in a pinch", s16 damage)
 
 SINGLE_BATTLE_TEST("Blaze boosts Fire-type moves outside of pinch", s16 damage)
 {
-u16 hp;
-PARAMETRIZE { hp = 99; }
-PARAMETRIZE { hp = 99; }
-GIVEN {
-ASSUME(gMovesInfo[MOVE_EMBER].type == TYPE_FIRE);
-PLAYER(SPECIES_CHARMANDER) { Ability(ABILITY_BLAZE); MaxHP(99); HP(hp); }
-OPPONENT(SPECIES_WOBBUFFET);
-} WHEN {
-TURN { MOVE(player, MOVE_EMBER); }
-} SCENE {
-HP_BAR(opponent, captureDamage: &results[i].damage);
-} FINALLY {
-EXPECT_MUL_EQ(results[0].damage, Q_4_12(1.2), results[1].damage);
-}
+    u16 hp;
+    u16 ability;
+
+    PARAMETRIZE {ability = ABILITY_SHIELD_DUST; hp = 33; }
+    PARAMETRIZE {ability = ABILITY_BLAZE; hp = 99; }
+
+    GIVEN {
+        ASSUME(gMovesInfo[MOVE_EMBER].type == TYPE_FIRE);
+        PLAYER(SPECIES_CHARMANDER) { Ability(ability); MaxHP(99); HP(hp); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_EMBER); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_MUL_EQ(results[0].damage, Q_4_12(1.2), results[1].damage);
+    }
 }
