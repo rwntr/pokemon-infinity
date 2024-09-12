25c0759ba2ceaa5a15e85d4a2c67c9a2c4d26198
ru <ruairiwinter@gmail.com>
2024-09-12 01:06:08 -0500

    Implemented testing for Power Fists, Pyromancy, and Whiteout

---
+++ test/battle/ability/power_fists.c
@@ -0,0 +1,60 @@
+#include "global.h"
+#include "test/battle.h"
+
+
+SINGLE_BATTLE_TEST("Power Fists causes 20% increased punching move damage", s16 damage)
+{
+    u32 move1, move2;
+    u16 ability;
+    PARAMETRIZE { ability = ABILITY_ANTICIPATION; }
+    PARAMETRIZE { ability = ABILITY_POWER_FISTS; }
+    ASSUME(gMovesInfo[MOVE_ICE_PUNCH].type == TYPE_ICE);
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ability);}
+        OPPONENT(SPECIES_WOBBUFFET) {Defense(50); SpDefense(50);}
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_SPLASH); MOVE(player, MOVE_ICE_PUNCH); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_MUL_EQ(results[0].damage, Q_4_12(1.2), results[1].damage);
+    }
+}
+
+SINGLE_BATTLE_TEST("Power Fists calculates damage based on opponent SpDef", s16 damage)
+{
+    u32 move1, move2;
+    u16 ability;
+    PARAMETRIZE { ability = ABILITY_ANTICIPATION; }
+    PARAMETRIZE { ability = ABILITY_POWER_FISTS; }
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ability);}
+        OPPONENT(SPECIES_WOBBUFFET) {Defense(50); SpDefense(25);}
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_SPLASH); MOVE(player, MOVE_ICE_PUNCH); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        //stacking base 1.2x damage buff with expected 2x damage due to enemy SpDef being 1/2 of enemy Def, results in 2.4x
+        EXPECT_MUL_EQ(results[0].damage, Q_4_12((2*1.2)), results[1].damage);
+    }
+}
+
+SINGLE_BATTLE_TEST("Power Fists is unaffected by Ice Scales", s16 damage)
+{
+    u32 move1, move2;
+    u16 ability;
+    PARAMETRIZE { ability = ABILITY_ANTICIPATION; }
+    PARAMETRIZE { ability = ABILITY_ICE_SCALES; }
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_POWER_FISTS);}
+        OPPONENT(SPECIES_WOBBUFFET) { Ability(ability);}
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_SPLASH); MOVE(player, MOVE_ICE_PUNCH); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_EQ(results[0].damage, results[1].damage);
+    }
+}
+
---
+++ test/battle/ability/whiteout.c
@@ -0,0 +1,39 @@
+#include "global.h"
+#include "test/battle.h"
+
+
+SINGLE_BATTLE_TEST("Whiteout increases Ice move damage by 50% in hail", s16 damage)
+{
+    u32 move1, move2;
+    PARAMETRIZE { move1 = MOVE_SPLASH; move2 = MOVE_ICE_PUNCH; }
+    PARAMETRIZE { move1 = MOVE_HAIL; move2 = MOVE_ICE_PUNCH; }
+    ASSUME(gMovesInfo[MOVE_ICE_PUNCH].type == TYPE_ICE);
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_WHITEOUT);}
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponent, move1); MOVE(player, move2); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_MUL_EQ(results[0].damage, Q_4_12(1.5), results[1].damage);
+    }
+}
+
+SINGLE_BATTLE_TEST("Whiteout increases Ice move damage by 30% in snow", s16 damage)
+{
+    u32 move1, move2;
+    PARAMETRIZE { move1 = MOVE_SPLASH; move2 = MOVE_ICE_PUNCH; }
+    PARAMETRIZE { move1 = MOVE_SNOWSCAPE; move2 = MOVE_ICE_PUNCH; }
+    ASSUME(gMovesInfo[MOVE_ICE_PUNCH].type == TYPE_ICE);
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_WHITEOUT);}
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponent, move1); MOVE(player, move2); }
+    } SCENE {
+        HP_BAR(opponent, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_MUL_EQ(results[0].damage, Q_4_12(1.3), results[1].damage);
+    }
+}
