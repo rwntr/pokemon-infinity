37133a1ccba983bb7d28c70ded8e2b3113a04d55
ru <fuwaru@pm.me>
2024-11-12 18:20:19 -0600

    Raising Nidoqueen's SpA invalidated a few test conditions, fixed

--- test/battle/ai/ai.c
+++ test/battle/ai/ai.c
@@ -141,7 +141,7 @@ AI_SINGLE_BATTLE_TEST("AI prefers moves which deal more damage instead of moves 
     abilityAtk = ABILITY_NONE;
 
     // Scald and Poison Jab take 3 hits, Waterfall takes 2.
-    PARAMETRIZE { move1 = MOVE_WATERFALL; move2 = MOVE_SCALD; move3 = MOVE_POISON_JAB; move4 = MOVE_WATER_GUN; expectedMove = MOVE_WATERFALL; turns = 2; }
+    PARAMETRIZE { move1 = MOVE_WATERFALL; move2 = MOVE_SCALD; move3 = MOVE_POISON_JAB; move4 = MOVE_WATER_GUN; expectedMove = MOVE_SCALD; turns = 2; }
     // Poison Jab takes 3 hits, Water gun 5. Immunity so there's no poison chip damage.
     PARAMETRIZE { move1 = MOVE_POISON_JAB; move2 = MOVE_WATER_GUN; expectedMove = MOVE_POISON_JAB; abilityDef = ABILITY_IMMUNITY; turns = 3; }
 
@@ -359,11 +359,11 @@ AI_SINGLE_BATTLE_TEST("AI won't use ground type attacks against flying type Poke
         AI_FLAGS(AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY | AI_FLAG_TRY_TO_FAINT);
         PLAYER(SPECIES_CROBAT);
         PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_NIDOQUEEN) { Moves(MOVE_EARTHQUAKE, MOVE_TACKLE, MOVE_POISON_STING, MOVE_GUST); }
+        OPPONENT(SPECIES_NIDOQUEEN) { Moves(MOVE_EARTH_POWER, MOVE_TACKLE, MOVE_POISON_STING, MOVE_GUST); }
     } WHEN {
-        TURN { NOT_EXPECT_MOVE(opponent, MOVE_EARTHQUAKE); }
-        TURN { MOVE(player, MOVE_GRAVITY); NOT_EXPECT_MOVE(opponent, MOVE_EARTHQUAKE); }
-        TURN { EXPECT_MOVE(opponent, MOVE_EARTHQUAKE); SEND_OUT(player, 1); }
+        TURN { NOT_EXPECT_MOVE(opponent, MOVE_EARTH_POWER); }
+        TURN { MOVE(player, MOVE_GRAVITY); NOT_EXPECT_MOVE(opponent, MOVE_EARTH_POWER); }
+        TURN { EXPECT_MOVE(opponent, MOVE_EARTH_POWER); SEND_OUT(player, 1); }
     } SCENE {
         MESSAGE("Gravity intensified!");
     }
